091f708725cd6fdf4e9a624ddbdac19a300c584f
Dawid Weglarz <dawid.weglarz95@gmail.com>
2023-09-18 23:22:35 +0200

    Adds some style to the button and restore some of the functionalities

--- public/script.js
+++ public/script.js
@@ -1,33 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const logForm = document.getElementById("logForm");
-    const userNameInput = document.getElementById("userName");
-    const logList = document.getElementById("log");
+document.addEventListener("DOMContentLoaded", function() {
+  const logForm = document.getElementById("logForm");
+  const userNameInput = document.getElementById("userName");
+  const logList = document.getElementById("log");
 
-    // Function to fetch and update the log data
-    function updateLog() {
-        fetch("/log")
-            .then(response => response.json())
-            .then(data => {
-                logList.innerHTML = ""; // Clear the existing list
-                data.forEach(entry => {
-                    const listItem = document.createElement("li");
-                    listItem.textContent = entry;
-                    logList.appendChild(listItem);
-                });
-            });
-    }
+  // Function to fetch and update the log data
+  function updateLog() {
+    fetch("/log")
+      .then(response => response.json())
+      .then(data => {
+        logList.innerHTML = ""; // Clear the existing list
+        data.forEach(entry => {
+          const listItem = document.createElement("li");
+          listItem.textContent = entry;
+          logList.appendChild(listItem);
+        });
+      });
+  }
 
-    // Update the log when the page loads
-    updateLog();
+  // Update the log when the page loads
+  updateLog();
 
-    logForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const userName = userNameInput.value;
+  logForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let userName = userNameInput.value;
 
-        fetch(`/log?userName=${userName}`, { method: "POST" })
-            .then(() => {
-                userNameInput.value = ""; // Clear the input field
-                updateLog(); // Update the log after the button press
-            });
-    });
+    fetch(`/log?userName=${userName}`, { method: "POST" })
+      .then(() => {
+        userNameInput.value = ""; // Clear the input field
+        updateLog(); // Update the log after the button press
+      });
+  });
 });
