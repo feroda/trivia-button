20b25e7cdddda0f998ea9f11715756a8d453b8fe
Dawid Weglarz <dawid.weglarz95@gmail.com>
2023-09-21 11:47:06 +0200

    Fixes some style issues and backend logic

--- public/script.js
+++ public/script.js
@@ -1,15 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const logForm = document.getElementById("logForm");
   const userNameInput = document.getElementById("userName");
   const logList = document.getElementById("log");
+  const button = document.getElementById("submitButton");
 
   // Function to fetch and update the log data
   function updateLog() {
     fetch("/log")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         logList.innerHTML = ""; // Clear the existing list
-        data.forEach(entry => {
+
+        // Retrive only the last 10 entries
+        data = data.slice(-10);
+
+        // Invert the order of the entries
+        data.reverse();
+
+        // Add each entry to the list
+        data.forEach((entry) => {
           const listItem = document.createElement("li");
           listItem.textContent = entry;
           logList.appendChild(listItem);
@@ -20,14 +29,37 @@ document.addEventListener("DOMContentLoaded", function() {
   // Update the log when the page loads
   updateLog();
 
-  logForm.addEventListener("submit", function(e) {
+  logForm.addEventListener("submit", function (e) {
     e.preventDefault();
     let userName = userNameInput.value;
 
-    fetch(`/log?userName=${userName}`, { method: "POST" })
-      .then(() => {
-        userNameInput.value = ""; // Clear the input field
-        updateLog(); // Update the log after the button press
-      });
+    // If the user name is empty, add an error class to the input field
+    if (userName === "") {
+      userNameInput.classList.add("error");
+      setTimeout(function () {
+        userNameInput.classList.remove("error");
+      }, 3000);
+
+      return;
+    } else {
+      // Make field read-only
+      userNameInput.setAttribute("readonly", true);
+    }
+
+    // If the button is disabled, don't do anything
+    if (button.classList.contains("disabled")) {
+      return;
+    }
+
+    // Turn of button for 5 seconds
+    button.classList.add("disabled");
+    setTimeout(function () {
+      button.classList.remove("disabled");
+    }, 5000);
+
+    // Send request to server
+    fetch(`/log?userName=${userName}`, { method: "POST" }).then(() => {
+      updateLog(); // Update the log after the button press
+    });
   });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // Import body-parser
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true })); // Enable body-parser
@@ -9,23 +9,23 @@ app.use(bodyParser.urlencoded({ extended: true })); // Enable body-parser
 const log = [];
 
 app.get("/log", (req, res) => {
-    res.json(log);
+  res.json(log);
 });
 
 app.post("/log", (req, res) => {
-    const userName = req.body.userName; // Use req.body to access form data
-    const timestamp = new Date().toLocaleTimeString('en-US', { hour12: false });
+  const userName = req.query.userName || "Anonymous";
+  const timestamp = new Date().toLocaleTimeString("en-US", { hour12: false });
 
-    if (userName === "admin") {
-        log.length = 0;
-    } else {
-        const entry = `User ${userName} pressed the button at ${timestamp}`;
-        log.push(entry);
-    }
+  if (userName === "admin") {
+    log.length = 0;
+  } else {
+    const entry = `${userName} ha premuto alle ${timestamp}!`;
+    log.push(entry);
+  }
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
